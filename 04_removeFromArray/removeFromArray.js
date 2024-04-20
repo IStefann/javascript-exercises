7e6c8fa8e52f9198560e2212661d57bf8004fdab
IStefann <118936693+IStefann@users.noreply.github.com>
2024-04-20 16:54:20 +0200

    Completed Exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+    let newArray = [];
+    let i;
+    for (i = 0; i < array.length; i++) {
+        if (!args.includes(array[i]))
+            newArray.push(array[i]);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
